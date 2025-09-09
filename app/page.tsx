ed64a29a061332e10203025db34786e55c3441c1
Nived <nived@globalsurf.ae>
2025-09-09 10:33:52 +0530

    style:logo margin

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
       <Image src="/slide1.jpg" alt="slide1" fill />
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-      <Image src="/logo.svg" alt="logo" width={220} height={58} className="z-10 invert-100"/>
+      <Image src="/logo.svg" alt="logo" width={220} height={58} className="z-10 invert-100 mb-13"/>
       <div className="z-10 flex flex-col items-center justify-center gap-2 text-center">
       <div className="text-white text-xl font-semibold">Innovate. Illuminate. Inspire</div>
       <div className="text-white text-lg font-medium max-sm:text-sm">We’re switching on to a new experience soon!</div>
